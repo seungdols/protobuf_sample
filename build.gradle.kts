5b54faa9c1fc29f8af0fb767393be67c06245208
seungdols <seungdols0822@gmail.com>
2023-07-12 18:35:51 +0900

    Init settings

---
+++ build.gradle.kts
@@ -0,0 +1,75 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.9.0"
+    id("com.google.protobuf") version "0.9.3"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+ext["grpcVersion"] = "1.56.1"
+ext["protobufVersion"] = "3.23.4"
+ext["grpcKotlinVersion"] = "1.3.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // https://mvnrepository.com/artifact/com.google.protobuf/protobuf-kotlin
+    implementation("com.google.protobuf:protobuf-kotlin:${rootProject.ext["protobufVersion"]}")
+    // https://mvnrepository.com/artifact/io.grpc/grpc-protobuf
+    implementation("io.grpc:grpc-protobuf:${rootProject.ext["grpcVersion"]}")
+    // https://mvnrepository.com/artifact/io.grpc/grpc-kotlin-stub
+    runtimeOnly("io.grpc:grpc-kotlin-stub:${rootProject.ext["grpcKotlinVersion"]}")
+    // https://mvnrepository.com/artifact/org.apache.tomcat/annotations-api
+    compileOnly("org.apache.tomcat:annotations-api:6.0.53")
+
+    testImplementation(kotlin("test"))
+}
+
+sourceSets {
+    getByName("main") {
+        java {
+            srcDirs(
+                "build/generated/source/proto/main/java",
+                "build/generated/source/proto/main/kotlin",
+            )
+        }
+    }
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:${rootProject.ext["protobufVersion"]}"
+    }
+    plugins {
+        create("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:${rootProject.ext["grpcVersion"]}"
+        }
+        create("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:${rootProject.ext["grpcKotlinVersion"]}:jdk8@jar"
+        }
+    }
+    // omitted protoc and plugins config
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                create("grpc")
+                create("grpckt")
+            }
+            it.builtins {
+                create("kotlin")
+            }
+        }
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "protobuf_sample"
+
